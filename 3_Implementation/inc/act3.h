b934415f99f7fc19419544bc5cd9cfff8f4ac1d0
saurabh kumar <94218113+charlie-25@users.noreply.github.com>
2021-11-30 21:21:05 +0530

    Update act3.h

--- 3_Implementation/inc/act3.h
+++ 3_Implementation/inc/act3.h
@@ -1,1 +1,32 @@
+#ifndef __ACTIVITY_3_H__
+#define __ACTIVITY_3_H__
 
+
+/**
+ * Macro Definitions
+ */
+#define PWM_20_PERCENT (205) /**< This is for Output PWM for 20% duty cycle */
+#define PWM_40_PERCENT (410) /**< This is for Output PWM for 40% duty cycle */
+#define PWM_70_PERCENT (717) /**< This is for Output PWM for 70% duty cycle */
+#define PWM_95_PERCENT (973) /**< This is for Output PWM for 95% duty cycle */
+
+/**
+ * Include files
+ */
+#include <util/delay.h>
+#include <avr/io.h>
+
+/**
+ * Function Definitions
+ */
+
+/**
+ * @brief Function to Initialize Timer1 registers
+ *
+ */
+void Init_Timer();
+
+
+void PWM(uint16_t temp);
+
+#endif 
